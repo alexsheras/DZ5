f4f09d6a66e558efd784062f39529fa78d921c4d
ALEX SHER <sheras85@mail.com>
2023-01-02 14:23:33 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+        Random rnd = new Random(DateTime.Now.Millisecond);
+int[] msv = (new int[10]).Select(i => rnd.Next(-5, 6)).ToArray();
+Console.WriteLine("Исходный массив:");
+foreach (int i in msv) Console.Write("{0} ", i);
+double count = 0;
+for (int i = 0; i < msv.Length; ++i)
+    {
+        if (msv[i] % 2 != 0)
+            count += msv[i];
+    }
+    Console.Write("\n\nCуммa элементов массива с нечетными номерами : {0}\n\n", count);
+ 
